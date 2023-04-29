8f11937bb92c844069d2419fb3ce6d2da1e03b2a
alkarpova <sashulja01@gmail.com>
2023-04-29 20:54:50 +0300

    Added Filament Comment Resource

---
+++ app/Filament/Resources/CommentResource.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CommentResource\Pages;
+use App\Filament\Resources\CommentResource\RelationManagers;
+use App\Models\Comment;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CommentResource extends Resource
+{
+    protected static ?string $model = Comment::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('post_id')
+                    ->relationship('post', 'name', function (Builder $query) {
+                        return $query->select('posts.id', 'post_translations.name')
+                            ->leftJoin('post_translations', function ($join) {
+                                $join->on('posts.id', '=', 'post_translations.post_id')
+                                    ->where('post_translations.locale', '=', 'lv');
+                            });
+                    })
+                    ->required(),
+                Forms\Components\Select::make('user_id')
+                    ->relationship('user', 'name'),
+                Forms\Components\TextInput::make('name')
+                    ->maxLength(255),
+                Forms\Components\Textarea::make('message')
+                    ->required()
+                    ->maxLength(65535),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('post.name:lv'),
+                Tables\Columns\TextColumn::make('user.name'),
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime(),
+            ])
+            ->filters([
+                Tables\Filters\SelectFilter::make('post')
+                    ->relationship('post', 'name', function (Builder $query) {
+                        return $query->select('posts.id', 'post_translations.name')
+                            ->leftJoin('post_translations', function ($join) {
+                                $join->on('posts.id', '=', 'post_translations.post_id')
+                                    ->where('post_translations.locale', '=', 'lv');
+                            });
+                    }),
+                Tables\Filters\SelectFilter::make('user')
+                    ->relationship('user', 'name'),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListComments::route('/'),
+            'create' => Pages\CreateComment::route('/create'),
+            'edit' => Pages\EditComment::route('/{record}/edit'),
+        ];
+    }
+}
