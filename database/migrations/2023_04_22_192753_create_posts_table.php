f6fe96e7f0a6442822a90ec181bf2e269ecadc7c
alkarpova <sashulja01@gmail.com>
2023-04-29 19:52:54 +0300

    Fix softDeletes in Post

--- database/migrations/2023_04_22_192753_create_posts_table.php
+++ database/migrations/2023_04_22_192753_create_posts_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->string('image')->nullable();
             $table->boolean('status')->index()->default(true);
+            $table->softDeletes();
             $table->timestamps();
         });
     }
