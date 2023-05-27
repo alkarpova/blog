288ecf9d1cb333bfa670a8a2674d78ac8ba1758d
alkarpova <sashulja01@gmail.com>
2023-05-27 23:06:23 +0300

    Change width container in Filament

--- config/filament.php
+++ config/filament.php
@@ -239,7 +239,7 @@ return [
         'footer' => [
             'should_show_logo' => true,
         ],
-        'max_content_width' => null,
+        'max_content_width' => 'full',
         'notifications' => [
             'vertical_alignment' => 'top',
             'alignment' => 'right',
